7759348eab7aed425eab4c3b3028116bbef25ea9
Marco Mercad <mmerc007@ucr.edu>
2019-10-25 23:59:40 -0700

    added adding test and implementation

--- src/test/java/app/calculator/suitTest.java
+++ src/test/java/app/calculator/suitTest.java
@@ -5,12 +5,6 @@ import org.junit.Test;
 
 public class suitTest {
 	@Test
-	public void simpleTest() {
-		int a = 1;
-		int b = 2;
-		assertTrue(a +b == 3);
-	}
-	@Test
 	public void TestAdding() {
 		Calculator temp = new Calculator();
 		assertTrue(temp.Add(10, 3) == 10+3);
