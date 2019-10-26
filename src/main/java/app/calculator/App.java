71f1dd47a67821d9a9a23d8da2da3426d76f52ae
Marco Mercad <mmerc007@ucr.edu>
2019-10-26 16:20:00 -0700

    product

--- src/main/java/app/calculator/App.java
+++ src/main/java/app/calculator/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "testing the World!" );
+        System.out.println( "Calculator Deliver." );
     }
 }
